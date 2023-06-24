4def8b867ca2e6537141f6a16ab1a34dd79cafc7
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-06-24 00:58:05 +0000

    Built a basic card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suitRNG = Math.floor(Math.random() * suits.length);
+  console.log(suitRNG);
+  let rankRNG = Math.floor(Math.random() * ranks.length);
+  console.log(rankRNG);
+
+  document.querySelector("#rank").innerHTML = ranks[rankRNG];
+  if (suitRNG === 0 || suitRNG === 2) document.querySelector("#rank").setAttribute("class", "text-danger");
+  document.querySelector("#img-1").setAttribute("src", suits[suitRNG]);
+  document.querySelector("#img-2").setAttribute("src", suits[suitRNG]);
 };
+
+const ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+const heart = `https://www.adda52.com/blog/wp-content/uploads/2013/10/adda52-heart-suit-card_poker-150x150.png`;
+const club = `https://www.adda52.com/blog/wp-content/uploads/2013/10/club-suit-card-poker-adda52.png`;
+const diamond = `https://www.adda52.com/blog/wp-content/uploads/2013/10/diamond-suit-card-poker-adda52.png`;
+const spade = `https://www.adda52.com/blog/wp-content/uploads/2013/10/spade-suit-card-poker-adda52.png`;
+
+const suits = [heart, club, diamond, spade];
